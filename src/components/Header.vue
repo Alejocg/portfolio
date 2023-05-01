d91d35254d884b632ca52c76e9adf9d9acc11cb3
Alejo <alejoceretogomez@gmail.com>
2023-05-01 13:52:46 +0200

    margin

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -46,18 +46,18 @@
         </div>
         <div class="hidden md:flex items-center justify-end md:flex-1 lg:w-0">
           <a
-            href="/projects"
+            href="/"
             class="font-medium text-white hover:text-gray-300 px-4 py-2 rounded-md"
             >Projects</a
           >
        
           <a
-            href="/contact"
+            href="https://www.linkedin.com/in/alejocg"
             class="font-medium text-white hover:text-gray-300 px-4 py-2 mr-3 rounded-md"
             >Contact</a
           >
           <a
-            href="https://www.linkedin.com/in/"
+            href="https://www.linkedin.com/in/alejocg"
             target="_blank"
             class="inline-flex items-center justify-center px-4 py-2 border border-transparent text-base leading-6 font-medium rounded-md text-white bg-blue-600 hover:bg-blue-500 focus:outline-none focus:border-blue-700 focus:shadow-outline-blue active:bg-blue-700 transition ease-in-out duration-150"
           >
@@ -72,7 +72,7 @@
     <div :class="{ block: isOpen, hidden: !isOpen }" class="hidden md:hidden">
       <div class="px-2 pt-2 pb-3 sm:px-3">
         <a
-          href="/projects"
+          href="/"
           class="font-medium text-white hover:text-gray-300 block px-3 py-2 rounded-md"
           >Projects</a
         >
@@ -88,7 +88,7 @@
           class="font-medium text-white hover:text-gray-300 block px-3 py-2 rounded-md"
         >
         
-          <span>Linkedin.</span>
+          <span>Linkedin</span>
         </a>
       </div>
     </div>
--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -3,11 +3,11 @@
     <div class="container mx-auto py-12 lg:py-24">
       <div class="max-w-4xl mx-auto text-center">
         <h1
-          class="text-4xl md:text-5xl lg:text-6xl font-bold leading-tight text-transparent bg-gradient-to-r from-white to-gray-100 bg-clip-text"
+          class="text-3xl md:text-4xl lg:text-5xl font-bold leading-tight text-transparent bg-gradient-to-r from-white to-gray-100 bg-clip-text"
         >
           Welcome to my Portfolio!
         </h1>
-        <p class="text-lg mt-6 mb-12 text-white">
+        <p class="text-lg mt-4 mb-8 text-white">
           My name is Alejo and I'm a Product Owner with a passion for creating
           small projects that challenge me and help me learn new skills.
           Although I'm not a developer by trade, I enjoy exploring new
